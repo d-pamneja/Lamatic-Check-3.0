3af781a10e368c4ec97db1e934dc2dd6d47cbbcc
Dhruv Pamneja <100751844+d-pamneja@users.noreply.github.com>
2025-11-03 23:05:50 +0530

    [LAMATIC-COMMIT] Synced all flows to github

---
+++ lamatic/flows/WeakCountry.ts
@@ -0,0 +1,172 @@
+const flowConfig = {
+  "id": "434ea47a-eee9-4386-a1fa-e155e69af9a5",
+  "name": "finKesari Chat Trial",
+  "edges": [
+    {
+      "id": "triggerNode_1-LLMNode_706",
+      "type": "defaultEdge",
+      "source": "triggerNode_1",
+      "target": "LLMNode_706",
+      "sourceHandle": "bottom",
+      "targetHandle": "top"
+    },
+    {
+      "id": "LLMNode_706-responseNode_triggerNode_1",
+      "type": "defaultEdge",
+      "source": "LLMNode_706",
+      "target": "responseNode_triggerNode_1",
+      "sourceHandle": "bottom",
+      "targetHandle": "top"
+    },
+    {
+      "id": "response-trigger_triggerNode_1",
+      "type": "responseEdge",
+      "source": "triggerNode_1",
+      "target": "responseNode_triggerNode_1",
+      "sourceHandle": "to-response",
+      "targetHandle": "from-trigger"
+    }
+  ],
+  "status": "active",
+  "created_at": "2025-05-22T18:33:16.475115+00:00",
+  "slug": "WeakCountry",
+  "trigger_id": null,
+  "nodes": [
+    {
+      "id": "triggerNode_1",
+      "data": {
+        "modes": {},
+        "nodeId": "chatTriggerNode",
+        "values": {
+          "chat": "",
+          "domains": [
+            "*"
+          ],
+          "nodeName": "Chat Widget",
+          "chatConfig": {
+            "botName": "Lamatic Bot",
+            "imageUrl": "https://img.freepik.com/premium-vector/robot-android-super-hero_111928-7.jpg?w=826",
+            "position": "right",
+            "policyUrl": "https://lamatic.ai/docs/legal/privacy-policy",
+            "displayMode": "popup",
+            "placeholder": "Compose your message",
+            "suggestions": [
+              "What is lamatic?",
+              "How do I add data to my chatbot?",
+              "Explain this product to me"
+            ],
+            "errorMessage": "Oops! Something went wrong. Please try again.",
+            "hideBranding": false,
+            "primaryColor": "#ef4444",
+            "headerBgColor": "#000000",
+            "greetingMessage": "Hi, I am Lamatic Bot. Ask me anything about Lamatic",
+            "headerTextColor": "#FFFFFF",
+            "showEmojiButton": true,
+            "suggestionBgColor": "#f1f5f9",
+            "userMessageBgColor": "#FEF2F2",
+            "agentMessageBgColor": "#f1f5f9",
+            "suggestionTextColor": "#334155",
+            "userMessageTextColor": "#d12323",
+            "agentMessageTextColor": "#334155"
+          }
+        },
+        "trigger": true
+      },
+      "type": "triggerNode",
+      "measured": {
+        "width": 218,
+        "height": 95
+      },
+      "position": {
+        "x": 0,
+        "y": 0
+      },
+      "selected": false
+    },
+    {
+      "id": "responseNode_triggerNode_1",
+      "data": {
+        "label": "Response",
+        "nodeId": "chatResponseNode",
+        "values": {
+          "content": "{{LLMNode_706.output.generatedResponse}}",
+          "nodeName": "Chat Response",
+          "references": "",
+          "webhookUrl": "",
+          "webhookHeaders": ""
+        },
+        "isResponseNode": true
+      },
+      "type": "responseNode",
+      "measured": {
+        "width": 218,
+        "height": 95
+      },
+      "position": {
+        "x": 0,
+        "y": 300
+      },
+      "selected": false
+    },
+    {
+      "id": "LLMNode_706",
+      "data": {
+        "label": "dynamicNode node",
+        "modes": {},
+        "nodeId": "LLMNode",
+        "values": {
+          "tools": [],
+          "prompts": [
+            {
+              "id": "187c2f4b-c23d-4545-abef-73dc897d6b7b",
+              "role": "system",
+              "content": "You are an AI Assistant. Answer the user query as well as you can. You are also given a chat history to maintain and understand context."
+            },
+            {
+              "id": "187c2f4b-c23d-4545-abef-73dc897d6b7d",
+              "role": "user",
+              "content": "USER QUERY : {{triggerNode_1.output.chatMessage}}  \nCHAT HISTORY FOR CONTEXT : {{triggerNode_1.output.chatHistory}}"
+            }
+          ],
+          "memories": "[]",
+          "messages": "[]",
+          "nodeName": "Generate Text",
+          "attachments": "",
+          "credentials": "",
+          "generativeModelName": {
+            "type": "generator/text",
+            "params": {},
+            "model_name": "gemini/gemini-2.5-flash-lite",
+            "credentialId": "308dd7f8-576e-4b2b-b3b8-796d6ca1b5a0",
+            "provider_name": "gemini",
+            "credential_name": "DP_GEMINI"
+          }
+        }
+      },
+      "type": "dynamicNode",
+      "measured": {
+        "width": 218,
+        "height": 95
+      },
+      "position": {
+        "x": 0,
+        "y": 150
+      },
+      "selected": false
+    }
+  ]
+};
+
+export async function getNodesAndEdges(): Promise<{
+    nodes: Record<string, any>[],
+    edges: Record<string, any>[],
+}> {
+    return {
+        nodes: flowConfig.nodes,
+        edges: flowConfig.edges,
+    }
+}
+
+export async function getFlowConfig(): Promise<Record<string, any>> {
+    return flowConfig;
+}
