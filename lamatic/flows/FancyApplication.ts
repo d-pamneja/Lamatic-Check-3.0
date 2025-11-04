725d8027e151da7d359406d441987ee643ce99b7
Dhruv Pamneja <dpamneja@gmail.com>
2025-11-04 22:20:18 +0530

    [LAMATIC-COMMIT] Created flow

---
+++ lamatic/flows/FancyApplication.ts
@@ -0,0 +1,32 @@
+const flowConfig = {
+  "id": "d28a5a76-8f9c-4169-91d8-ea4593bac0d2",
+  "name": "Fancy Application",
+  "nodes": [
+    {
+      "id": "triggerNode_1",
+      "data": {},
+      "type": "triggerNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    }
+  ],
+  "edges": [],
+  "status": "active",
+  "created_at": "2025-11-04T16:50:17.220015+00:00"
+};
+
+export async function getNodesAndEdges(): Promise<{
+    nodes: Record<string, any>[],
+    edges: Record<string, any>[],
+}> {
+    return {
+        nodes: flowConfig.nodes,
+        edges: flowConfig.edges,
+    }
+}
+
+export async function getFlowConfig(): Promise<Record<string, any>> {
+    return flowConfig;
+}
