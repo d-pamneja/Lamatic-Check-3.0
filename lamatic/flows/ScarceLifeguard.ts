deecaf0ae60b78a1e8397adca4c866f88b01bc43
Dhruv Pamneja <dpamneja@gmail.com>
2025-11-04 12:56:31 +0530

    [LAMATIC-COMMIT] Created flow

---
+++ lamatic/flows/ScarceLifeguard.ts
@@ -0,0 +1,32 @@
+const flowConfig = {
+  "id": "4887fcc1-8a38-4105-b0fa-dae605a1abbf",
+  "name": "Scarce Lifeguard",
+  "nodes": [
+    {
+      "id": "triggerNode_1",
+      "data": {},
+      "type": "triggerNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    }
+  ],
+  "edges": [],
+  "status": "active",
+  "created_at": "2025-11-04T07:26:30.391529+00:00"
+};
+
+export async function getNodesAndEdges(): Promise<{
+    nodes: Record<string, any>[],
+    edges: Record<string, any>[],
+}> {
+    return {
+        nodes: flowConfig.nodes,
+        edges: flowConfig.edges,
+    }
+}
+
+export async function getFlowConfig(): Promise<Record<string, any>> {
+    return flowConfig;
+}
