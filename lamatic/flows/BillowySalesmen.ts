4fa92c9350b21a308e8e0253e2268044da12c034
Dhruv Pamneja <dpamneja@gmail.com>
2025-11-04 22:13:39 +0530

    [LAMATIC-COMMIT] Created flow

---
+++ lamatic/flows/BillowySalesmen.ts
@@ -0,0 +1,32 @@
+const flowConfig = {
+  "id": "95236a11-3502-460c-8e6e-3900024b90a3",
+  "name": "Billowy Salesmen",
+  "nodes": [
+    {
+      "id": "triggerNode_1",
+      "data": {},
+      "type": "triggerNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    }
+  ],
+  "edges": [],
+  "status": "active",
+  "created_at": "2025-11-04T16:43:37.779036+00:00"
+};
+
+export async function getNodesAndEdges(): Promise<{
+    nodes: Record<string, any>[],
+    edges: Record<string, any>[],
+}> {
+    return {
+        nodes: flowConfig.nodes,
+        edges: flowConfig.edges,
+    }
+}
+
+export async function getFlowConfig(): Promise<Record<string, any>> {
+    return flowConfig;
+}
