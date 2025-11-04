4d374324d21e2b1b8b1257be06f854777f3d2fd9
Dhruv Pamneja <dpamneja@gmail.com>
2025-11-04 22:28:16 +0530

    [LAMATIC-COMMIT] Created flow

---
+++ lamatic/flows/EnoughHouse.ts
@@ -0,0 +1,32 @@
+const flowConfig = {
+  "id": "b0343abc-d6c3-409e-8680-63b7cfce0604",
+  "name": "Enough House",
+  "nodes": [
+    {
+      "id": "triggerNode_1",
+      "data": {},
+      "type": "triggerNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    }
+  ],
+  "edges": [],
+  "status": "active",
+  "created_at": "2025-11-04T16:58:14.420622+00:00"
+};
+
+export async function getNodesAndEdges(): Promise<{
+    nodes: Record<string, any>[],
+    edges: Record<string, any>[],
+}> {
+    return {
+        nodes: flowConfig.nodes,
+        edges: flowConfig.edges,
+    }
+}
+
+export async function getFlowConfig(): Promise<Record<string, any>> {
+    return flowConfig;
+}
