27433c72c8df3d7ca66005aa86432273090b61df
Dhruv Pamneja <dpamneja@gmail.com>
2025-11-04 13:49:31 +0530

    [LAMATIC-COMMIT] Created flow

---
+++ lamatic/flows/QuaintJackal.ts
@@ -0,0 +1,32 @@
+const flowConfig = {
+  "id": "2fc06f7b-2e5e-4861-8c1e-bd7fc3a45caa",
+  "name": "Quaint Jackal",
+  "nodes": [
+    {
+      "id": "triggerNode_1",
+      "data": {},
+      "type": "triggerNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    }
+  ],
+  "edges": [],
+  "status": "active",
+  "created_at": "2025-11-04T08:19:30.474729+00:00"
+};
+
+export async function getNodesAndEdges(): Promise<{
+    nodes: Record<string, any>[],
+    edges: Record<string, any>[],
+}> {
+    return {
+        nodes: flowConfig.nodes,
+        edges: flowConfig.edges,
+    }
+}
+
+export async function getFlowConfig(): Promise<Record<string, any>> {
+    return flowConfig;
+}
