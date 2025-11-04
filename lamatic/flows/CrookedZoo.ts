514bb8000be4ad6bd8f916dd7e7e577016d06392
Dhruv Pamneja <dpamneja@gmail.com>
2025-11-04 12:54:17 +0530

    [LAMATIC-COMMIT] Created flow

---
+++ lamatic/flows/CrookedZoo.ts
@@ -0,0 +1,32 @@
+const flowConfig = {
+  "id": "f5dfbdf8-0e08-40f9-9ff2-1091399afc51",
+  "name": "Crooked Zoo",
+  "nodes": [
+    {
+      "id": "triggerNode_1",
+      "data": {},
+      "type": "triggerNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    }
+  ],
+  "edges": [],
+  "status": "active",
+  "created_at": "2025-11-04T07:24:17.233941+00:00"
+};
+
+export async function getNodesAndEdges(): Promise<{
+    nodes: Record<string, any>[],
+    edges: Record<string, any>[],
+}> {
+    return {
+        nodes: flowConfig.nodes,
+        edges: flowConfig.edges,
+    }
+}
+
+export async function getFlowConfig(): Promise<Record<string, any>> {
+    return flowConfig;
+}
