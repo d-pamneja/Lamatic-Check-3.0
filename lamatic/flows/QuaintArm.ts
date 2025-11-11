91d980e9bd952db14ac47b29f8ba3496a645c11d
Dhruv Pamneja <dpamneja@gmail.com>
2025-11-11 13:50:37 +0530

    [LAMATIC-COMMIT] Created flow

---
+++ lamatic/flows/QuaintArm.ts
@@ -0,0 +1,32 @@
+const flowConfig = {
+  "id": "5a2e2027-dc9f-41e2-87d7-dad5dc2d2cab",
+  "name": "Quaint Arm",
+  "nodes": [
+    {
+      "id": "triggerNode_1",
+      "data": {},
+      "type": "triggerNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    }
+  ],
+  "edges": [],
+  "status": "active",
+  "created_at": "2025-11-11T08:20:36.554787+00:00"
+};
+
+export async function getNodesAndEdges(): Promise<{
+    nodes: Record<string, any>[],
+    edges: Record<string, any>[],
+}> {
+    return {
+        nodes: flowConfig.nodes,
+        edges: flowConfig.edges,
+    }
+}
+
+export async function getFlowConfig(): Promise<Record<string, any>> {
+    return flowConfig;
+}
